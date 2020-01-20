a9575104f73d4cde4771bc5372f45d850b32b4b2
sunxiaomingATcn <767694331@qq.com>
2020-01-20 17:40:13 +0800

    Update api.js

--- app/controller/api.js
+++ app/controller/api.js
@@ -1,9 +1,14 @@
 module.exports = app => {
     return class ApiController extends app.Controller {
+        async articleList() {
+            const {ctx, service} = this;
+            this.ctx.body = await service.http.request('/api/articleList?pageNum=1&pageSize=30');
+        }
+        
         async wxConfig() {
             const {ctx, service} = this;
             const page = ctx.query.page;
-            this.ctx.body = await service.http.request('/rest/api/wechat/js/config?page=' + page);
+            this.ctx.body = await service.http.request('/api/config?page=' + page);
         }
     };
 };
