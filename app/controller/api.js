958a8124506423be460098ad0ec89223bcbb107b
@sunxm <sunxm@cheche365.com>
2020-01-20 18:01:05 +0800

    添加接口

--- app/controller/api.js
+++ app/controller/api.js
@@ -1,9 +1,15 @@
 module.exports = app => {
     return class ApiController extends app.Controller {
         async articleList() {
-            const {ctx, service} = this;
+            const {service} = this;
             this.ctx.body = await service.http.request('/api/articleList?pageNum=1&pageSize=30');
         }
+
+        async baiduSearch() {
+            const {service} = this;
+            this.ctx.body = await service.http.request('/sugrec?bs=egg-vue-ssr');
+        }
+
         // ....
     };
 };
--- app/router/api.js
+++ app/router/api.js
@@ -1,3 +1,4 @@
 module.exports = app => {
     app.get('/api/articleList',app.controller.api.articleList);
+    app.get('/api/baidu_search', app.controller.api.baiduSearch)
 };
